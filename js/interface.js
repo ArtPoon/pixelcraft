098d27a386e3c14cfcbe83323afc56c974d0c9dc
ArtPoon <artpoon@gmail.com>
2014-07-22 00:05:04 -0300

    Enabled PDF exports.  Closes issue #6.  Closes issue #1. Now requires jsPDF library. HTML5 Canvas contents now reproduced in PDF format by native  jsPDF drawing functions.

--- js/interface.js
+++ js/interface.js
@@ -139,7 +139,7 @@ function decoratePixel (ctx, x, y, key, invcol) {
         ctx.moveTo(x+shim, y+shim);
         ctx.lineTo(x+pixel_width-shim, y+pixel_width-shim);
         ctx.stroke();
-    } else if (key == '1') { // upper-right quadrant
+    } else if (key == '1') { // upper-left quadrant
         ctx.fillRect(x, y, half, half);
     } else if (key == '2') {
         ctx.fillRect(x+half, y, half, half);
@@ -147,6 +147,70 @@ function decoratePixel (ctx, x, y, key, invcol) {
         ctx.fillRect(x+half, y+half, half, half);
     } else if (key == '4') {
         ctx.fillRect(x, y+half, half, half);
+    }
+}
+
+
+function decoratePixelPDF(doc, x, y, key, rgbStr) {
+    /*
+    Decorate pixel on PDF using jsPDF.
+     */
+    var half = pixel_width/2.,
+        shim = pixel_width/8.,
+        hypo = Math.sqrt(2) * half,
+        rgb = rgbStr.split(',').map(function(x) {return+x;});
+        invcol = invertColour(rgbStr),
+        invrgb = invcol.split(',').map(function(x) {return+x;});
+
+    doc.setLineWidth(1.5);
+    doc.setDrawColor(invrgb[0], invrgb[1], invrgb[2]);
+    doc.setFillColor(invrgb[0], invrgb[1], invrgb[2]);
+    if (key === '0') {
+        doc.circle(x+half, y+half, 0.6*half, 'F');
+    } else if (key === 'o') {
+        doc.circle(x+half, y+half, 0.6*half, 'D')
+    } else if (key === 'D') {
+        doc.circle(x+half, y+half, 0.8*half, 'F');
+        doc.setFillColor(rgb[0], rgb[1], rgb[2]);
+        doc.rect(x, y, half, pixel_width, 'F');
+    } else if (key === 'U') {
+        doc.circle(x+half, y+half, 0.8*half, 'F');
+        doc.setFillColor(rgb[0], rgb[1], rgb[2]);
+        doc.rect(x, y, pixel_width, half, 'F');
+    } else if (key === '^') { // semi-circle up
+        doc.circle(x+half, y+half, 0.8*half, 'F');
+        doc.setFillColor(rgb[0], rgb[1], rgb[2]);
+        doc.rect(x, y+half, pixel_width, half, 'F');
+    } else if (key === 'd') { // semi-circle left
+        doc.circle(x+half, y+half, 0.8*half, 'F');
+        doc.setFillColor(rgb[0], rgb[1], rgb[2]);
+        doc.rect(x+half, y, half, pixel_width, 'F');
+    } else if (key === 'v') { // triangle down
+        doc.triangle(x+shim, y+shim, x+pixel_width-shim, y+shim, x+half, y+half, 'F');
+    } else if (key === 'A') { // triangle up
+        doc.triangle(x+shim, y+pixel_width-shim, x+pixel_width-shim, y+pixel_width-shim, x+half, y+half, 'F');
+    } else if (key === '+') {
+        doc.lines([[pixel_width-2*shim, 0]], x+shim, y+half);
+        doc.lines([[0, pixel_width-2*shim]], x+half, y+shim);
+    } else if (key === '-') {
+        doc.lines([[pixel_width-2*shim, 0]], x+shim, y+half);
+    } else if (key === '|') {
+        doc.lines([[0, pixel_width-2*shim]], x+half, y+shim);
+    } else if (key === '/') {
+        doc.lines([[hypo, -hypo]], x+shim, y+pixel_width-shim);
+    } else if (key === '\\') {
+        doc.lines([[hypo, hypo]], x+shim, y+shim);
+    } else if (key === 'x') {
+        doc.lines([[hypo, -hypo]], x+shim, y+pixel_width-shim);
+        doc.lines([[hypo, hypo]], x+shim, y+shim);
+    } else if (key === '1') {  // upper left quadrant fill
+        doc.rect(x, y, half, half, 'F');
+    } else if (key === '2') {
+        doc.rect(x+half, y, half, half, 'F');
+    } else if (key === '3') {
+        doc.rect(x+half, y+half, half, half, 'F');
+    } else if (key === '4') {
+        doc.rect(x, y+half, half, half, 'F');
     }
 }
 
@@ -274,4 +338,125 @@ function updateCanvas (value) {
         }
         colour_key.push(map);
     }
+}
+
+
+function exportPDF() {
+    /*
+    use jsPDF (http://parall.ax/products/jspdf) to generate a multipage
+    PDF based on objects being displayed in HTML5 Canvas.
+    Client-side generated PDF is saved using browser download interface.
+     */
+
+    // initialize PDF document
+    var doc = new jsPDF('landscape', 'pt', 'letter');
+    doc.setFont('helvetica', 'normal');
+    doc.setFontSize(12);
+
+    var mapped, rgb,
+	    mode = document.getElementById('dropdown').value,
+	    x, y,
+        decorate_keys = 'v1A-dU^o+2D03/4\\|';
+
+    // cache colour inversions and decoration keys
+    var i,  // counter
+        key,
+        value = $('#ncolSlider').val(),
+        cache = {},
+        keys = Object.keys(palettes[mode][value]);
+
+    for (i = 0; i < keys.length; i++) {
+        key = keys[i];
+        cache[key] = {};
+        cache[key]['inv'] = invertColour(palettes[mode][value][key]);
+        cache[key]['dec'] = decorate_keys[i];
+    }
+
+    // write each page to PDF
+    var page, origin_x, origin_y,
+        v, row = 0, col = 0;
+
+    for (page = 0; page < num_pages; page++) {
+        if (page > 0) {
+            doc.addPage(); // next page
+        }
+
+        origin_x = pixels_per_page * Math.floor(page / pagesWide),
+        origin_y = pixels_per_page * (page % pagesWide);
+
+        // draw pixel grid
+        for (row = origin_x; row < origin_x + pixels_per_page; row++) {
+            if (row >= imageData.height) {
+                // past limit of image
+                break;
+            }
+            for (col = origin_y; col < origin_y + pixels_per_page; col++) {
+                if (col >= imageData.width) {
+                    // past end of row in image
+                    break;
+                }
+
+                // imageData.data is a vector indexed as (row*ncol + col)
+                i = 4 * (row * imageData.width + col);
+                // skip transparent pixels
+                if (imageData.data[i+3] == 0) {
+                    continue;
+                }
+
+                // adjust row, col to canvas
+                x = pixel_margin + (col % pixels_per_page) * pixel_step + Math.floor(col / pixels_per_grid) * grid_step;
+                y = pixel_margin + (row % pixels_per_page) * pixel_step + Math.floor(row / pixels_per_grid) * grid_step;
+
+                // draw pixel
+                if (mode === 'rgb') {
+                    doc.setFillColor(imageData.data[i], imageData.data[i+1], imageData.data[i+2]);
+                } else {
+                    mapped = lookupRGB(imageData.data[i], imageData.data[i+1], imageData.data[i+2], mode);
+                    doc.setFillColor(mapped.r, mapped.g, mapped.b);
+                }
+
+                doc.rect(x, y, pixel_width, pixel_width, 'F');
+
+                if ($('#decorate_pixel_checkbox').prop('checked')) {
+                    // original colour
+                    key = [imageData.data[i], imageData.data[i+1], imageData.data[i+2]].toString();
+                    v = cache[key];
+                    decoratePixelPDF(doc, x, y, v['dec'], key);
+                }
+            }
+        }
+
+        // draw legend
+        var colour_key = [],
+            colours = Object.values(palettes['rgb'][value]),
+            map;
+
+        for (i = 0; i < colours.length; i++) {
+            // from original colour space
+            rgb = colours[i].split(',').map(function(x){return+x;});
+            if (mode === 'rgb') {
+                doc.setFillColor(rgb[0], rgb[1], rgb[2]);
+            } else {
+                map = lookupRGB(rgb[0], rgb[1], rgb[2], mode);
+                if (colour_key.indexOf(map) > -1) {
+                    // already in colour key
+                    continue;
+                }
+                doc.setFillColor(map.r, map.g, map.b);
+            }
+
+            x = pixels_per_page * pixel_step + 2*pixel_margin + pixels_per_page / pixels_per_grid * grid_step;
+            y = pixel_margin + legend_step * i;
+
+            doc.rect(x, y, pixel_width, pixel_width, 'F');
+            if ($('#decorate_pixel_checkbox').prop('checked')) {
+                v = cache[keys[i]];
+                decoratePixelPDF(doc, x, y, v['dec'], colours[i]);
+            }
+            doc.text(map.index + ' ' + map.name, x + pixel_step, y + pixel_width - 1);
+            colour_key.push(map);
+        }
+    }
+
+    doc.save('export.pdf');
 }
